88f17a6fa28e02e609f22682442c727e8cca28cb
Randy Weyer <randy@>
2018-09-05 09:06:48 -0700

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>Something</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from "./components/App";
+import Header from "./components/Header";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('react-app-root')
+);
